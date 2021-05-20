a806ca5dffcb98dde251686d05cf740672f8e636
Siljesandersen <silje_sandersen@hotmail.com>
2021-05-20 16:45:32 +0200

    update recipe.js

--- js/recipe.js
+++ js/recipe.js
@@ -7,14 +7,14 @@ async function getDetails(recipeId) {
         const response = await fetch('https://noroffcors.herokuapp.com/'+'http://api.siljes.tech/wp-json/wp/v2/posts/' + recipeId);
         const result = await response.json();
         console.log(result);
-        
-        for (let i = 0; i < result.content.length; i++){
-            console.log(result.content[i])
-            document.querySelector('main') += `
-            <div>
-                ${result.content[i].rendered}
-            </div>`
-        };
+        console.log(result.content.rendered)
+
+            document.querySelector('main').innerHTML += `
+            <h1>${result.title.rendered}</h1>
+            <section>
+                ${result.content.rendered}
+            </section>`
+
     } catch (error) {
         document.querySelector('.alert').innerHTML += showAlertToUser('An error occured','danger');
     } finally {
